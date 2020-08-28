b5aad0c8c8b09c1641f5cecc8714b5423ac12b91
Tim Grohmann <timgrohmann@icloud.com>
2020-08-28 11:02:33 +0200

    updates to landing page

--- vue-frontend/src/components/LandingPage.vue
+++ vue-frontend/src/components/LandingPage.vue
@@ -1,11 +1,11 @@
 <template>
-  <v-card>
+  <div>
     <v-card-title class="d-none">
       <div class="headline grey darken-1">Das wird timgrohmann.de - BAUSTELLE</div>
     </v-card-title>
     <v-layout row wrap justify-center>
       <v-flex xs12 md8 align-self-center>
-        <v-card-text class="display-2 ">
+        <v-card-text class="display-2">
           <span class="display-4 secondary--text">Hey,</span> ich bin Tim.
         </v-card-text>
       </v-flex>
@@ -20,7 +20,7 @@
       </v-flex>
     </v-layout>
     <v-card-text>
-      <v-layout row wrap align-center justify-center hidden-sm-and-down>
+      <v-layout row wrap align-center justify-left justify-sm-center hidden-sm-and-down>
         <v-flex class="text-xs-left text-md-right no-grow">
           <!--span class="display-4 secondary--text hidden-md-and-up"></span-->
           <span class="display-1">Ich bin</span>
@@ -29,33 +29,45 @@
           <DottedLine />
         </v-flex>
         <v-flex class="no-grow">
-          <span class="display-4 secondary--text">{{ age }}</span>
-          <span class="display-1">Jahre alt.</span>
+          <span class="display-4 secondary--text animate texts-t">Full Stack</span>
+          <br />
+          <span class="display-1 animate texts-s">Entwickler.</span>
         </v-flex>
       </v-layout>
       <v-layout row wrap align-center justify-center hidden-md-and-up>
         <v-flex class="no-grow">
           <span class="display-1">Ich bin</span>
-          <span class="display-4 secondary--text">{{ age }}</span>
-          <span class="display-1">Jahre alt.</span>
+          <br />
+          <span class="display-3 secondary--text animate texts-t">Full Stack</span>
+          <br />
+          <span class="display-1 animate texts-s">Entwickler.</span>
         </v-flex>
       </v-layout>
     </v-card-text>
     <v-card-text>
-      Finde mich auf:
+      Du suchst nach persönlicher und kompetenter Unterstützung bei deinem IT-Projekt?
+    </v-card-text>
+    <v-card-text>
+      <p>
+      Dann kontaktiere mich <a href="mailto:mail@timgrohmann.de">per Mail</a> oder finde mich auf:
+      </p>
       <div class="social-links">
-        <a href="https://instagram.com/timgrohmann">
+        <a href="https://instagram.com/timgrohmann" target="_blank">
           <img src="/img/timgrohmann_nametag.png" alt />
           <span>Instagram</span>
         </a>
-        <a href="https://twitter.com/_just_timothy_">
+        <a href="https://twitter.com/_just_timothy_" target="_blank">
           <img src="/img/twitter.png" alt />
           <span>Twitter</span>
+        </a>
+        <a href="https://www.linkedin.com/in/tim-grohmann" target="_blank">
+          <img src="/img/LinkedIn.png" alt />
+          <span>LinkedIn</span>
         </a>
       </div>
     </v-card-text>
     <v-card-text>Was ich kann, was ich nicht kann, man weiß es nicht</v-card-text>
-  </v-card>
+  </div>
 </template>
 
 <script>
@@ -66,7 +78,27 @@ export default {
     DottedLine,
   },
   data() {
-    return {};
+    return {
+      texts: [
+        {
+          t: "Full Stack",
+          s: "Entwickler.",
+        },
+        {
+          t: "jung",
+          s: "und innovativ.",
+        },
+        {
+          t: "bereit",
+          s: "für Dein Projekt.",
+        },
+      ],
+      timer: null,
+      state: 0,
+    };
+  },
+  created() {
+    this.timer = setInterval(this.cycle, 5000);
   },
   computed: {
     age() {
@@ -75,6 +107,38 @@ export default {
         new Number((new Date().getTime() - birthday.getTime()) / 31536000000)
       );
     },
+  },
+  methods: {
+    cycle() {
+      this.state++;
+      if (this.state == this.texts.length) {
+        this.state = 0;
+      }
+      console.log("Timer", this.state);
+      Array.from(document.getElementsByClassName("animate")).forEach((e) => {
+        e.classList.add("animate-out");
+      });
+      setTimeout(() => {
+        Array.from(document.getElementsByClassName("animate")).forEach((e) => {
+          e.classList.remove("animate-out");
+          e.classList.add("animate-in");
+        });
+        Array.from(document.getElementsByClassName("texts-t")).forEach((e) => {
+          e.innerHTML = this.texts[this.state].t;
+        });
+        Array.from(document.getElementsByClassName("texts-s")).forEach((e) => {
+          e.innerHTML = this.texts[this.state].s;
+        });
+      }, 100);
+      setTimeout(() => {
+        Array.from(document.getElementsByClassName("animate")).forEach((e) => {
+          e.classList.remove("animate-in");
+        });
+      }, 200);
+    },
+  },
+  beforeDestroy() {
+    clearInterval(this.timer);
   },
 };
 </script>
@@ -97,11 +161,28 @@ export default {
     display: flex;
     flex-direction: column;
     align-items: center;
+    margin: 0 5px;
   }
   img {
     width: 100%;
     max-width: 128px;
     height: auto;
   }
+}
+
+.animate {
+  transition: all ease-in 0.1s;
+  opacity: 1;
+  transform: translateY(0);
+  display: inline-block;
+}
+.animate-out {
+  transform: translateY(-10px);
+  opacity: 0;
+}
+.animate-in {
+  transition: all ease-out 0.1s;
+  transform: translateY(10px);
+  opacity: 0;
 }
 </style>
